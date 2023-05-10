5a7e5ba8bf0fa89d64ac9e133459b71b915d0341
couplewith <couplewith@gmail.com>
2023-05-11 08:13:10 +0900

    update

---
+++ python_study/web_crawler/README.md
@@ -0,0 +1,2 @@
+* Web Crawling with Selenium
+* 셀레니움을 이용한 웹크롤링
--- python_study/web_crawler/get_sitemap_selenium_like_select.py
+++ python_study/web_crawler/get_sitemap_selenium_like_select.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
 
 from bs4 import BeautifulSoup
 from time import sleep
