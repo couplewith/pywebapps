06106efb358a13b7d562e44222985ac1f2ce2f34
couplewith <couplewith@gmail.com>
2026-08-26 00:32:06 +0900

    Add files via upload

--- python_study/web_crawler/blog_search_url_couplewith.py
+++ python_study/web_crawler/blog_search_url_couplewith.py
@@ -11,12 +11,13 @@ def url_encode(text):
 
 # 테스트
 key_words = []
-key_words.extend(["기술동향","디지털기술","인공지능","IT트렌드", "Trends", "디지털+트렌스포메이션", "Digital+Transformation"])
-key_words.extend(["오늘의명언"])
+key_words.extend(["인공지능","AI비즈니스","AI&Tech","AI 에이전트","AI챗봇","RAG","자가진화"])
+key_words.extend(["퀀텀 양자","쳇봇 RAG","IT트렌드", "Trends", "디지털+트렌스포메이션", "Digital+Transformation"])
+key_words.extend(["오늘의명언","명언","전략기획","경영전략","성공 전략"])
 key_words.extend(["빅데이터","database","블록체인","Data+mesh", "SQL", "SQLite"])
-key_words.extend(["리눅스","셀스크립트","정규표현식","원도우+파일복구"])
-key_words.extend(["블록체인","스마트컨트렉트","토큰증권","ERC-1400"])
-key_words.extend(["selenium+기초","셀레니움", "Python","파이썬+프로그래밍기초","Rust+프로그래밍" ])
+key_words.extend(["리눅스", "Linux 튜닝", "Linux","셀스크립트","정규표현식","원도우+파일복구"])
+key_words.extend(["블록체인","스마트컨트렉트","금융","STO","ERC-1400"])
+key_words.extend(["selenium+기초","셀레니움", "테스트 자동화","Python","파이썬+프로그래밍기초","Rust+프로그래밍" ])
 
 # WebDriver 초기화
 driver = webdriver.Chrome()  # chromedriver 경로를 지정해야 합니다.
@@ -48,7 +49,6 @@ for keyword in key_words:
         search_url = f"https://www.google.com/search?q={encoded_keyword}+site%3Acouplewith.tistory.com&sourceid=chrome{idx}&ie=UTF-8"
 
         driver.get(search_url)
-        time.sleep(1)
 
     else:
 
@@ -64,9 +64,14 @@ for keyword in key_words:
 
         # 검색을 실행합니다.
         search_box.send_keys(Keys.RETURN)
-        time.sleep(1)
+        
 
     try:
+        
+        if idx <3:
+            time.sleep(30)
+        else:
+            time.sleep(1)
 
         # 검색 결과 링크 가져오기
         # #rso > div:nth-child(1) > div > div > div > div > div > span > a
@@ -75,36 +80,61 @@ for keyword in key_words:
         # // XPath 표현식은 ID가 "rso"인 모든 /div 요소를 선택한다.
 
         links = driver.find_elements(By.XPATH, '//*[@id="rso"]//div//a')
-        print( " >> search result find links", len(links))
+        links_len=len(links)
+        print( f'[{idx}]>> search result find links [{links_len}]' )
 
         for link in links:
             url = link.get_attribute('href')
             attr = link.get_attribute('text')
             url_lists.append({"url": url, "attr": attr, "keyword" : keyword})
+        
+        url_cnt=len(url_lists)
 
-        print(" -> ", keyword, len(url_lists))
+        print(f' >>keyword [{keyword}]--> url_lists_cnt:{url_cnt}')
 
     except NoSuchElementException:
         print(">  NoSuchElementException ", search_url)
 
+#################
+print("[Url Scan Done] -> url_lists ", len(url_lists), url_lists)
 
-#
-print(" -> url_lists ", len(url_lists), url_lists)
-# URL 리스트 출력
-#  url_lists[ [url, attr, keyword],... ]
+#################
+# URL 리스트 출력 :  url_lists[ [url, attr, keyword],... ]
+#################
+
 idx = 0
+tabid = 0
+tabid_bef = 0
 
 for url in url_lists:
     
     idx = idx + 1
-
     tabid = (idx % 3)
+
     driver.switch_to.window(driver.window_handles[tabid])
-    print(idx, tabid, url)
+    print(idx, tabid, tabid_bef, url)
 
     driver.get(url['url'])
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    driver.switch_to.window(driver.window_handles[tabid_bef])
+
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # 페이지를 키워드로 스크롤 합니다.
+    for c in range(0, 2):
+        # 페이지를 아래 위로 스크롤 합니다.
+        driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
+        time.sleep(1)
+        driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_UP)
+        time.sleep(1)
+        driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
+    
+
+
     time.sleep(1)
+    driver.switch_to.window(driver.window_handles[tabid])
+    tabid_bef = tabid
+
+    print(f'Done>> idx=[{idx}]  tabid[{tabid}], tabid_bef[{tabid_bef}], url={url}')
 
 # WebDriver 종료
-driver.quit()
+#driver.quit()
